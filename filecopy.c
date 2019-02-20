35294a6e254e02ad32330e09ec2f0333b8fd43e3
stephlsechrist <stephlsechrist@gmail.com>
2019-02-20 15:43:30 -0800

    minimal work done so far

--- filecopy.c
+++ filecopy.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <printf.h> // included to use printf statements for prompts
 //You may also add more include directives as well.
 
 // THIS VALUE CANNOT BE CHANGED.
@@ -17,6 +18,22 @@
 int
 main(int argc, char const *argv[])
 {
+    char sizeOf[BUFF_MAX];
 
+    // char arrays for file names
+    // in slack channel, it was said that filenames
+    // at most 255 bytes, and paths 4096 bytes
+    char sourceFile[255];
+    char destinationFile[255];
+
+    printf("Welcome to the File Copy Program by Stephanie Sechrist!");
+    printf("Enter the name of the file to copy from: ");
+    scanf("%s", sourceFile);
+    printf("Enter the name of the file to copy to: ");
+    scanf("%s", destinationFile);
+
+
+    // needs conditional and TBD to be changed
+    printf("File Copy Successful, %d bytes copied", TBD);
     return 0;
 }
